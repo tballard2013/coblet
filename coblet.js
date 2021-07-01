8b0af60f59251a05ae3622c4c4928d796e46c1a4
Thomas Ballard <thomas.ballard@gmail.com>
2021-07-01 02:34:07 -0700

    expanding example

--- coblet.js
+++ coblet.js
@@ -5,36 +5,89 @@ export class Coblet {
     }) {
         const _el = document.querySelector(`${el}`);
         const html = `
-            NEW COBLET ${new Date().toLocaleTimeString()}
+            <span class="coblet-form">
+            ${metadata.title ? `<h1>${metadata.title}</h1>` : '<!-- no title -->'}
             <form>
-            ${this.form(metadata)}
+            ${this.buildFormRows(metadata)}
             </form>
+            </span>
         `;
 
         _el.innerHTML = html;
     }
 
-    form (metadata) {
-        let html = '';
+    buildFormRows (metadata) {
+        // build a tabular form from the metadata
+
+        let html = '<table class="coblet-form">';
         for (let idx in metadata.fields) {
-            html += '<div>'
             const word = metadata[metadata.fields[idx]];
-            switch (word.type) {
-                case 'text':
-                    html += `<input type="text" 
-                        name="${word.name}"
-                        placeholder="${word.placeholder}"
-                        value="${word.value}"
-                        >
-                    `
-                    break;
-                default:
-                        html += 'unknown word: ' + JSON.stringify(word, null, 4);
-            }
-            html += '</div>'
+            html += `<tr>
+                <td data-label="${word.label || 'no label provided'}">
+                    ${word.label || ''}
+                </td>
+                <td data-input="${word.type || 'no type provided'}">
+                    ${this.buildFormInput(word)}
+                </td>
+                <td data-message="${word.message || 'no message provided'}">
+                    ${word.message || ''}
+                </td>
+            </tr>`;
         }
 
+        // buttons for submit/cancel
+        if (metadata.submit || metadata.cancel) {
+            html += `
+                <tr>
+                    <td colspan="3" align="right">
+                        <input type="submit" 
+                            ${metadata.submit ? `value="${metadata.submit}"` : ''}
+                        >
+
+                        ${metadata.reset ? `
+                            <input type="reset">
+                        ` : '<!-- no reset button -->'}
+                    </td>
+                </tr>
+            `
+        }
+
+
+        html += '</table>';
+
         return html;
+    }
+
+    buildFormInput(word) {
+        let html = '';
+        switch (word.type) {
+            case 'text':
+                html += `<input type="text" 
+                    name="${word.name}"
+                    placeholder="${word.placeholder}"
+                    value="${word.value || ''}"
+                    >`
+                break;
+            case 'select':
+                html += `<select>
+                    ${this.buildOptions(word)}
+                    </select>
+                    `
+                break;
+            default:
+                    html += 'unknown word: ' + JSON.stringify(word, null, 4);
+        }
+        return html;
+
+    }
+
+    buildOptions(word) {
+        let html = '';
+        word.possibleValues.forEach( i => {
+            html += `<option>${i}</option>`
+        })
+        return html;
+
     }
 
 }
--- main.js
+++ main.js
@@ -4,16 +4,42 @@ window.init = (function () {
     const c = new Coblet({
         el: '#coblet', 
         metadata: {
-            fields: [
-                'firstName'
-            ],
+            title: 'Basic Form Example',
+            submit: 'Go', // string label to customize
+            reset: true, // string label or truthy to include cancel button
+
+            fields: [ 
+                'firstName',
+                'lastName',
+                'gender',
+            ], // the list (in order) of fields to present
             
             firstName: {
+                name: 'firstName', // match to key name for best results
+                label: 'First name',
                 type: 'text', 
-                placeholder: 'First name',
-                name: 'first',
+                placeholder: 'Thomas',
                 maxlength: 50,
+                required: true,
+            },
+
+            lastName: {
+                name: 'lastName',
+                label: 'Last name',
+                type: 'text',
+                placeholder: 'Ballard',
+            },
+
+            gender: {
+                name: 'gender',
+                label: 'Gender',
+                type: 'select',
+                possibleValues: [
+                    'prefer not to answer', 'male', 'female', 'other'
+                ],
+
             }
+            
         }
     });
 });
